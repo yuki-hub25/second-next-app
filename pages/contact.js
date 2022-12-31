26d948d4dd09caa7bcc6ce2fa37fe3ed8fcdc001
Yuki <r.yuki1125.putin@gmail.com>
2022-12-31 18:54:38 +0900

    width-chenge

--- pages/contact.js
+++ pages/contact.js
@@ -25,7 +25,7 @@ export default function Home() {
             <Head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=320, initial-scale=1.0" />
                 <title>portfolio</title>
             </Head>
             <header className={styles.header}>
